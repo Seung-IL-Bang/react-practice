a81e0e49d640b66777b8606e156e29942188ce04
Seungil <seungilbang@khu.ac.kr>
2023-03-21 17:30:52 +0900

    [UPD] dynamic list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
-  const expenses = [
-    { title: "e1", amount: 100, date: new Date(2021, 1, 12) },
-    { title: "e2", amount: 200, date: new Date(2021, 2, 12) },
-    { title: "e3", amount: 300, date: new Date(2021, 3, 12) },
-  ];
+const DUMMY_EXPENSES = [
+  { title: "e1", amount: 100, date: new Date(2021, 1, 12) },
+  { title: "e2", amount: 200, date: new Date(2021, 2, 12) },
+  { title: "e3", amount: 300, date: new Date(2021, 3, 12) },
+];
 
+function App() {
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,21 +16,13 @@ export default function Expenses(props) {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        {props.items.map((expense) => (
         <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
+      ))}
       </Card>
     </div>
   );
