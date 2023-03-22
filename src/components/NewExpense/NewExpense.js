f88103f59a270347189d1548319550779040d4c3
Bang <seungilbang@khu.ac.kr>
2023-03-22 22:46:12 +0900

    [UPD] before implementaing  Chart

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,10 @@
+import {useState} from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 
 export default function NewExpense(props) {
+
+  const [isEditing, setIsEditing] = useState(false);
   
   const saveExpenseData = (enteredExpenseData) => {
     const expenseData = {
@@ -10,11 +13,23 @@ export default function NewExpense(props) {
     }
     console.log(expenseData);
     props.onAddExpense(expenseData);
-  }
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseData}/>
+      {!isEditing && <button onClick={startEditingHandler}>Add new Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseData} onCancel={stopEditingHandler}/>}
     </div>
   );
 }
